c1d1e3bf3a167ec608ce471191450db0eea61f5e
Joshua Sands <83298011+Joshsands@users.noreply.github.com>
2023-01-27 16:29:13 -0600

    added navbar options

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,13 +7,15 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
+import { Badge } from 'react-bootstrap';
 
 
 function Navigation() {
   return (
-    <Navbar bg="danger" expand="lg">
+    <Navbar bg="primary" expand="lg" sticky='top'>
       <Container fluid>
-        <Navbar.Brand href="#" className='logo'>FireScale Design</Navbar.Brand>
+        
+        <Navbar.Brand href="#" className='logo'><h1>FireScale Design</h1></Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -21,21 +23,30 @@ function Navigation() {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#shop">Shop</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <NavDropdown title="Quote" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#design">
+                Design
               </NavDropdown.Item>
-              <NavDropdown.Divider />
+              <NavDropdown.Item href="#estimating">
+                Estimating
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#pm">
+                Project Management
+              </NavDropdown.Item>
+              {/* <NavDropdown.Divider />
               <NavDropdown.Item href="#action5">
                 Something else here
-              </NavDropdown.Item>
+              </NavDropdown.Item> */}
             </NavDropdown>
             <Nav.Link href="#" disabled>
-              Link
+              Profile
             </Nav.Link>
+            <Nav.Link href="#login">
+              Login
+              </Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
